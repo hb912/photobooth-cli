a6826c292fa5f4ebcf876a606bec044b74907928
chaehong kim <43055448+hb912@users.noreply.github.com>
2024-06-05 18:17:40 +0900

    feat: 헤더 컴포넌트 구현

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header>
+    <div class="header">
+      <div class="logo">
+        Photo Booth
+      </div>
+      <div class="icon">
+        <i class="fa fa-bell"/>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MainHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/fonts.scss";
+
+.header {
+  display: flex;
+  align-items: center;
+  height: 20px;
+  background-color: $main-color;
+  color: white;
+  padding: 13px 20px;
+  justify-content: space-between;
+
+}
+
+.logo {
+  font-size: 1.5rem;
+  //font-weight: bold;
+  font-family: 'Kaushan Script', sans-serif;
+}
+
+.icon {
+  font-size: 1.3rem;
+}
+
+</style>
